0adc8603171e9207027ae1d9de782e1778d2b55a
Leonardo Sampaio Ferraz Ribeiro <leo.sampaio.ferraz.ribeiro@gmail.com>
2016-11-30 17:42:43 -0200

    Fixed Telegram Silencing Challenge fixed group names

---
+++ Assets/MinigameAssets/TelegramSilence/MessageTitleCreator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageTitleCreator : MonoBehaviour {
+
+	private string[] possibleMessages = {
+		"Lena's Birthday",
+		"TUSCA",
+		"Gamenight",
+		"Semcomp42",
+		"Bananight",
+		"CodeClubClub",
+		"Ragnarokers",
+		"WOW",
+		"Monsters BIRL"
+	};
+
+	// Use this for initialization
+	void Start () {
+		int message = Random.Range (0, possibleMessages.Length);
+		transform.GetComponent<TextMesh> ().text = possibleMessages [message];
+	}
+}
