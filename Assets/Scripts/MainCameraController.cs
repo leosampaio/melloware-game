982c2ca6b5dd5d017814885b4d97760592068c66
Leonardo Sampaio Ferraz Ribeiro <leo.sampaio.ferraz.ribeiro@gmail.com>
2016-12-06 20:39:39 -0200

    Added Credits and fixed Fonts Bug

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -8,12 +8,19 @@ public class MainCameraController : MonoBehaviour {
 	private Vector3 menuCameraInitialPos;
 	private float menuCameraInitialSize;
 
+	private Vector3 creditsCameraPos;
+	private float creditsCameraSize;
+
 	public float smoothTime = 0.3F;
 	private Vector3 velocity = Vector3.zero;
 	private float zoomVelocity = 0f;
 
 	private bool focusingOnGame = false;
 	private bool focusingOutOfGame = false;
+	private bool focusingOnCredits = false;
+	private bool focusingOutOfCredits = false;
+
+	public GameObject creditsObjects;
 
 	public GameController gameController;
 
@@ -26,6 +33,9 @@ public class MainCameraController : MonoBehaviour {
 
 		gameCameraInitialPos = new Vector3 (0.05f, 0.2f, -10f);
 		gameCameraInitialSize = 10.7841f;
+
+		creditsCameraPos = new Vector3 (44f, 1.5f, -10f);
+		creditsCameraSize = 14f;
 	}
 	
 	// Update is called once per frame
@@ -37,22 +47,47 @@ public class MainCameraController : MonoBehaviour {
 				focusingOnGame = false;
 				gameController.startGame ();
 			}
-		} else if (focusingOutOfGame) {
+		} else if (focusingOutOfGame || focusingOutOfCredits) {
 			transform.position = Vector3.SmoothDamp (transform.position, menuCameraInitialPos, ref velocity, smoothTime);
 			Camera.main.orthographicSize = Mathf.SmoothDamp (Camera.main.orthographicSize, menuCameraInitialSize, ref zoomVelocity, smoothTime);
 			if (transform.position == menuCameraInitialPos) {
 				focusingOutOfGame = false;
+				focusingOutOfCredits = false;
+			}
+		} else if (focusingOnCredits) {
+			transform.position = Vector3.SmoothDamp (transform.position, creditsCameraPos, ref velocity, smoothTime);
+			Camera.main.orthographicSize = Mathf.SmoothDamp (Camera.main.orthographicSize, creditsCameraSize, ref zoomVelocity, smoothTime);
+			if (transform.position == creditsCameraPos) {
+				focusingOnCredits = false;
 			}
 		}
 	}
-
+	 
 	public void focusOnGame() {
 		focusingOnGame = true;
 		focusingOutOfGame = false;
+		focusingOnCredits = false;
+		focusingOutOfCredits = false;
 	}
 
 	public void focusOutOfGame() {
 		focusingOutOfGame = true;
 		focusingOnGame = false;
+		focusingOnCredits = false;
+		focusingOutOfCredits = false;
+	}
+
+	public void focusOnCredits() {
+		focusingOnGame = false;
+		focusingOutOfGame = false;
+		focusingOnCredits = true;
+		focusingOutOfCredits = false;
+		creditsObjects.SetActive(true);
+	}
+
+	public void focusOutOfCredits() {
+		focusingOnCredits = false;
+		focusingOutOfCredits = true;
+		creditsObjects.SetActive(false);
 	}
 }
